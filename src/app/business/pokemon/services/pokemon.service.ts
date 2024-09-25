12ac7a038f12f4369377cdb0b739833bcfb3858f
Alexander Quispe Alvi <alxnderq@gmail.com>
2024-09-25 00:38:58 -0500

    add changes

--- src/app/business/pokemon/services/pokemon.service.ts
+++ src/app/business/pokemon/services/pokemon.service.ts
@@ -4,19 +4,20 @@ import { Observable } from 'rxjs';
 import { Pokemon, PokemonList } from '../models';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class PokemonService {
 
-    constructor(
-        private httpClient: HttpClient
-    ) { }
+  constructor(
+    private httpClient: HttpClient
+  ) {
+  }
 
-    getAllPokemon(offset: number, limit: number): Observable<PokemonList> {
-        return this.httpClient.get<PokemonList>(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
-    }
+  getAllPokemon(offset: number, limit: number): Observable<PokemonList> {
+    return this.httpClient.get<PokemonList>(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
+  }
 
-    getPokemon(id_name: number | string): Observable<Pokemon> {
-        return this.httpClient.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${id_name}`);
-    };
+  getPokemon(id_name: number | string): Observable<Pokemon> {
+    return this.httpClient.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${id_name}`);
+  };
 }
